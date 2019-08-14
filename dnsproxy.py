f4aba9dd57b4ca4c8f0480067799ddce4fde6343
Nicklas Ring <nicklas@verendus.se>
2019-08-14 22:45:56 +0200

    Invalid packet
    Issues with response, dest unreachable and malformed packet

--- dnsproxy.py
+++ dnsproxy.py
@@ -1,14 +1,14 @@
 #!/usr/local/bin/python
 
 from socket import socket, AF_INET, SOCK_DGRAM, SOCK_STREAM, SHUT_WR
-from struct import unpack
+from struct import unpack, pack
 
 from dnsfirewall import DNSFirewall
 
 SERVER_ADDRESS = '127.0.0.1'
 DNS_PORT = 53
 DNS_SERVERS = [
-    "9.9.9.9" # Quad9
+    "1.1.1.1" # Quad9
 ]
 
 class DNSProxy():
@@ -31,21 +31,38 @@ class DNSProxy():
             response = relay_socket.recv(4096)
             self._socket.sendto(response, self._client_address)
             relay_socket.shutdown(SHUT_WR)
-
         except Exception as E:
             print(E)
 
     def RelayFake(self, data):
         try:
-            print('Relaying fake response')
+            transaction_id = unpack("!H", data[:2])[0]
+            flags = unpack("!H", data[2:4])[0]
+            questions = unpack("!H", data[4:6])[0]
+            answer_rrs = unpack("!H", data[6:8])[0]
+            authority_rrs = unpack("!H", data[8:10])[0]
+            additional_rrs = unpack("!H", data[10:12])[0]
+
+            dns_request_length = 12 + self.GetQnameLength()
+
+            request_header = data
+            response_flag = pack('>L', 8180)
+            
+            ip_addr =  tuple(map(int,'1.2.3.4'.rstrip(".").split(".")))
+            pad_byte = pack('!x')
+            answer = pack('!H', 6)+pad_byte+pack('!H', 4)+pad_byte+pack('!BBBB', *ip_addr)
+            response = pack('!H', transaction_id) + response_flag + request_header[4:]
+
+            self._socket.sendto(response, self._client_address)
         except Exception as E:
             print(E)
+            print('shit')
 
     def SetDNSPayload(self, payload):
         self._dns_payload = payload.split(b'\x00',1)
 
         q_data = unpack('!2H', self._dns_payload[1][0:4])
-
+        
         self.SetQType(q_data[0])
         self.SetQClass(q_data[1])
 
@@ -59,6 +76,9 @@ class DNSProxy():
         self.SetDNSPayload(client_data[12:])
         self._dns_query = self.GetDNSPayload()[0]
 
+    def SetQnameLength(self, length):
+        self._qname_length = length
+
     def GetQType(self):
         return self._dns_qtype
 
@@ -71,11 +91,16 @@ class DNSProxy():
     def GetDNSQuery(self):
         return self._dns_query
 
+    def GetQnameLength(self):
+        return self._qname_length
+
     def GetQname(self):
         b = len(self._dns_query)
         qname_length = b + 1
 
-        qname = unpack(f'!{b}B', self._dns_query[:qname_length])
+        self.SetQnameLength(qname_length)
+
+        qname = unpack(f'!{b}B', self._dns_query[:self.GetQnameLength()])
         length = qname[0]
         qname_raw = ''
         for byte in qname[1:]:
@@ -92,34 +117,17 @@ class DNSProxy():
     def Start(self):
         while True:
             client_data, self._client_address = self._socket.recvfrom(1024)
-            
-            transaction_id = unpack("!H", client_data[:2])[0]
-            flags = unpack("!H", client_data[2:4])[0]
-            questions = unpack("!H", client_data[4:6])[0]
-            answer_rrs = unpack("!H", client_data[6:8])[0]
-            authority_rrs = unpack("!H", client_data[8:10])[0]
-            additional_rrs = unpack("!H", client_data[10:12])[0]
 
             self.SetDNSQuery(
                 client_data
             )
 
-            qname = self.GetQname()
-
-            if not self._dns_firewall.IsAllowed(qname): 
-                print(f'---------\nDomain: {qname} is not allowed\n---------')
+            if not self._dns_firewall.IsAllowed(self.GetQname()): 
+                print(f'Responding with fake DNS for: {self.GetQname()}...')
                 self.RelayFake(client_data)
                 continue
             else:
-                qtype =  self.GetQType()
-                qclass = self.GetQClass()
-                
-                print('-----------------------------------------------------------')
-                print(
-                    f'Transaction ID: {transaction_id}\nFlags: {flags}\nQuestions: {questions}\nAnswer RRS: {answer_rrs}\nAuthority RRS {authority_rrs}\nAdditional RRS {additional_rrs}\nQuery Name: {qname}\nQuery Type: {qtype}\nQuery Class: {qclass}'''
-                )
-                print('Relaying DNS Query ...')
-                
+                print(f'Relaying DNS Query for: {self.GetQname()}...')
                 self.Relay(client_data)
 
 if __name__ == '__main__':
